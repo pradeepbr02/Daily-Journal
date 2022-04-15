1554f139e8340f1925edd6c75c9c08f8867a276e
Pradeep <pradeepbr177@gmail.com>
2022-04-15 22:35:08 +0530

    Included Database for the blogs

--- app.js
+++ app.js
@@ -4,26 +4,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/BlogDB", {useNewUrlParser:true});
+
+const blogSchema = new mongoose.Schema({
+  title:String,
+  content:String
+});
+
+const blog = mongoose.model("blog" , blogSchema);
+
+
+
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 // let array=[homeStartingContent,aboutContent,contactContent];
+
+
+
 const app = express();
 
-let array=[];
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  res.render("home",{
-    para:homeStartingContent,
-    array:array
+app.get("/" , (req,res)=>{
+  blog.find({} , (err,array)=>{
+    res.render("home" , {para : homeStartingContent, array:array});
   });
-  
 });
 
 app.get("/contact",function(req,res){
@@ -39,28 +55,37 @@ app.get("/compose",function(req,res){
 app.post("/compose",function(req,res){
  let pt=req.body.postTitle;
  let pd=req.body.postBody;
-  let post={
-    title:pt,
-    content:pd
-  }
+ const blog1= new blog ({
+   title:pt,
+   content:pd
+ });
+
+ blog1.save((err)=>{
+   if(!err){
+     res.redirect("/");
+   }
+ });
   
-  array.push(post);
-  res.redirect("/");
+  
 
 
 });
 
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle=_.lowerCase(req.params.postName);
+app.get("/posts/:postId",function(req,res){
+  const postId = req.params.postId;
+
+  blog.findOne({_id:postId} , function(err , post){
+    res.render("post", {
+      title : post.title,
+      Content : post.content
+    });
+  });
+
   
-  array.forEach(function(arr){
-    let m=_.lowerCase(arr.title);
-    if(m===requestedTitle){
-      res.render("post",{
-        title:arr.title,
-        Content:arr.content
-      });
-    }
+    
+ 
+  
+
     // if(m===requestedTitle){
     //   res.render("post",{
     //   subpostTitle: arr.title,
@@ -71,7 +96,7 @@ app.get("/posts/:postName",function(req,res){
   });
 
   
-});
+
 
 
 
@@ -85,3 +110,5 @@ PORT=process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
+
+
